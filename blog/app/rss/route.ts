dc60464906a1ca05e188bb002c56d8dfd9a504c0
Gitto Yong <gittoyong@gmail.com>
2024-09-21 02:25:11 +0900

    NextJs Blog setup

---
+++ blog/app/rss/route.ts
@@ -0,0 +1,45 @@
+import { baseUrl } from 'app/sitemap'
+import { getBlogPosts } from 'app/blog/utils'
+
+// baseURL/rss
+// RSS: Rich Site Summary 또는  Really Simple Syndication
+
+export async function GET() {
+  let allBlogs = await getBlogPosts()
+
+  const itemsXml = allBlogs // 날짜 별 정렬
+    .sort((a, b) => {
+      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+        return -1
+      }
+      return 1
+    })
+    .map(
+      (post) =>
+        `<item>
+          <title>${post.metadata.title}</title>
+          <link>${baseUrl}/blog/${post.slug}</link>
+          <description>${post.metadata.summary || ''}</description>
+          <pubDate>${new Date(
+            post.metadata.publishedAt
+          ).toUTCString()}</pubDate>
+        </item>`
+    )
+    .join('\n')
+
+  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+        <title>My Portfolio</title>
+        <link>${baseUrl}</link>
+        <description>This is my portfolio RSS feed</description>
+        ${itemsXml}
+    </channel>
+  </rss>`
+
+  return new Response(rssFeed, {
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  })
+}
