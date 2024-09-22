cb47bd281713c32dde2945520bc748d83c37f398
gitto <gittoyong@gmail.com>
2024-09-22 18:23:01 +0900

    post Life of Gitto

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -4,14 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Welcome to Gitto Blog !
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        Hello, My name is Gitto. I hope that you will gain useful information or knowledge from this. 
+        <br/>
+        <br/>
+        Enjoy Gitto Blog. ˆˆ
       </p>
       <div className="my-8">
         <BlogPosts />
